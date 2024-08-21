f8e7a3a91484d65fbead4679c04402ba070aedbe
Ellen Ruth <167252820+Gh0St-Nimos@users.noreply.github.com>
2024-08-21 09:12:52 -0300

    Update Rpg.js

--- Rpg.js
+++ Rpg.js
@@ -1,1 +1,9 @@
+window.onload = function() {
+    setTimeout(function() {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    }, 1000);
+};
 
